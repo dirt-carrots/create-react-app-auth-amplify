846a3743700d5e84bab3e928d67b7ff9b9c9ff88
dontokoi <tkcheng@gmail.com>
2021-02-06 18:16:25 -0600

    Text test

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <h1>Hello, l33tworld!</h1>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
